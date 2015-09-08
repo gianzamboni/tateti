171a11fe636104989626f238a501da288e6107d3
Gian150 <gianfranco376@gmail.com>
2015-09-08 15:11:57 -0300

    sa

---
+++ tateti_struct/Tateti.cpp
@@ -0,0 +1,96 @@
+#include "Tateti.h"
+#include <iostream>
+#include <random>
+using namespace std;
+
+Tateti::Tateti(){
+	table = new char*[3];
+	for(int i = 0; i < 3;i++){
+		table[i] = new char[3];
+		for(int j = 0; j<3;j++){
+			table[i][j] = 0;
+		}
+	}
+	mode = PVC;
+	activePlayer = get_player1();
+}
+
+Tateti::Tateti(MODE m) {
+	table = new char*[3];
+	for(int i = 0; i < 3;i++){
+		table[i] = new char[3];
+		for(int j = 0; j<3;j++){
+			table[i][j] = 0;
+		}
+	}
+	mode = m;
+	activePlayer = get_player1();
+}
+
+Tateti::~Tateti(){
+	for(int i = 0; i < 3;i++){
+		delete[] table[i];	 
+	}
+	delete[] table;
+}
+
+void Tateti::init_game(){
+
+	int n = 0;
+	while(n!=1){
+		n++;
+		int x;
+		int y;
+
+		cout<< "gimme me ya move "<< actual_player() << "!: ";
+		cin >> x >> y;
+		cout << endl;
+		while(not_a_valid_move(x,y)){
+			cout<<"stop being a cunt!:"<< endl;
+			cin >> x >> y;
+			cout << endl;
+		}
+
+		set_slot(x,y, activePlayer);
+		print_game();
+		activePlayer = !activePlayer;
+
+	}
+
+}
+
+bool Tateti::get_player1(){
+	default_random_engine generator;
+	uniform_int_distribution<int> distribution(0,1);
+	return (bool) distribution(generator);
+}
+
+bool Tateti::actual_player(){
+	// TODO
+	return true;
+}
+
+void Tateti::set_slot(int x, int y, bool actPlyr ){
+	actPlyr?(table[x][y]='X'):(table[x][y]='O');
+	//this.table[x][y] = (actPlyr? 'X' : 'O')	
+}
+
+bool Tateti::game_over(){
+	//TODO
+	//return no_valid_moves(0,0) || someone_won();
+	return true;
+}
+
+void Tateti::print_game(){
+	for(int col=0; col<3; col++){
+		for(int row=0; row<3; row++){
+			cout << (!not_a_valid_move(col,row)? '-': table[col][row]) << "\t";
+		}
+		cout << endl;
+	}
+}
+
+
+bool Tateti::not_a_valid_move(int x,int y){
+	return x>2 || y>2 || x<0 || y<0 || table[x][y] != 0;
+}
---
+++ tateti_struct/Tateti.h
@@ -0,0 +1,35 @@
+#ifndef _TATETI_H_
+#define _TATETI_H_
+
+#include <vector>
+using namespace std;
+
+enum MODE { PVP, PVC };
+
+class Tateti {
+
+public:
+		//Constructors
+		Tateti();
+		Tateti(MODE m);
+		~Tateti();
+
+		//Methods
+		bool actual_player();
+		bool game_over();
+		bool not_a_valid_move(int x, int y);
+
+		void init_game();
+		void print_game();
+		void set_slot(int x, int y, bool activePlayer);
+
+
+	private: 
+		char** table;
+		bool activePlayer;
+		MODE mode;
+
+		bool get_player1();
+
+};
+#endif
