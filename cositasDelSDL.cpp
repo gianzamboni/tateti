db1a0d6c7624d5c30e1ec153b666d854603f4bbd
Autumn Vulpes <ignacioviana@live.com>
2015-09-25 15:16:18 -0300

    "new p01.bmp img, use 24b/r8g8b8 for future imags. ;)

--- cositasDelSDL.cpp
+++ cositasDelSDL.cpp
@@ -5,7 +5,7 @@ SDL_Window* crear_tablero(){
 	SDL_Window* window = SDL_CreateWindow( "TATETI", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 320, 320, SDL_WINDOW_SHOWN);
 	//Get window surface
     SDL_Surface* screenSurface = SDL_GetWindowSurface( window );
-    SDL_Surface* fondo = SDL_LoadBMP("imgs/hello_world.bmp");
+    SDL_Surface* fondo = SDL_LoadBMP("imgs/p01.bmp");
 
     //Fill the surface white
    	SDL_BlitSurface( fondo, NULL, screenSurface, NULL );
